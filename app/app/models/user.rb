728c78e7f57ffcc16b0ff1c6f928f84601888a22
Brooke Hartley <82181143+brooke-hartley@users.noreply.github.com>
2023-08-15 16:11:25 -0400

    Added devise and reinit repo

---
+++ app/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Add the admin attribute
+  attr_accessor :admin
+end
